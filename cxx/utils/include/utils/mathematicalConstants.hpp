db24d2fbba755b44d0f0513f95f26020fa3769c5
Ali Bozorgzadeh <61201050+abzrg@users.noreply.github.com>
2024-08-22 17:31:58 +0330

    utils: Added mathematical constants
    For example: utils::constants::mathematical::pi

---
+++ cxx/utils/include/utils/mathematicalConstants.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cmath>
+
+namespace utils::constants::mathematical {
+
+constexpr double e(M_E);
+constexpr double pi(M_PI);
+constexpr double twoPi(2 * M_PI);
+constexpr double piByTwo(0.5 * M_PI);
+
+} // namespace utils::constants
+
+
